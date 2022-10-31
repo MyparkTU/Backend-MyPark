8617a4c266957f7d3af8c84f696ca4a6aa953c2e
Thanachot W <Thanachot.wongmetin@gmail.com>
2022-11-01 01:15:47 +0700

    Update

--- routes/DeviceRoutes.js
+++ routes/DeviceRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/", (request, response) => {
+router.get("/", (req, res) => {
     response.json({ message: "Hello This Device IoT Path" });
 });
 
