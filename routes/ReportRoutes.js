305dfaf3ea2ea26ad5303751ec23c19f47a39048
Thanachot W <Thanachot.wongmetin@gmail.com>
2022-11-04 13:45:05 +0700

    update

--- routes/ReportRoutes.js
+++ routes/ReportRoutes.js
@@ -5,17 +5,13 @@ const Report = require('../controllers/ReportController');
 router.get("/", (req, res) => {
     res.json({ message: "Hello This Report Path" });
 });
-
+//Hello
 router.get("/all", Report.findAll);
-
+//Hello
 router.post("/create", Report.create);
-
-//
-
 // for create newPlace to db
 router.post("/:id", Report.findByid);
-
 // get all data
 router.put("/:id", Report.update);
-
+//Get
 module.exports = router;
